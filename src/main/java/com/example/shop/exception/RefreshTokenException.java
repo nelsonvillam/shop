8b4d069be718dad9849ae81bfe7d96fe8fbf7342
Nelson Villamizar <nelsonvillam@egmail.com>
2026-06-27 16:00:36 -0400

    fix: add serialVersionUID to RefreshTokenException to satisfy PMD

--- src/main/java/com/example/shop/exception/RefreshTokenException.java
+++ src/main/java/com/example/shop/exception/RefreshTokenException.java
@@ -1,6 +1,8 @@
 package com.example.shop.exception;
 
 public class RefreshTokenException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
     public RefreshTokenException(String message) {
         super(message);
     }
